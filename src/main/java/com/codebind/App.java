51ac51f3077fbf55659cfe3ac45f2f2502ff3862
Ayazuddin <46484289+ayazuddin007@users.noreply.github.com>
2019-10-15 21:13:49 +0530

    create bug

--- src/main/java/com/codebind/App.java
+++ src/main/java/com/codebind/App.java
@@ -4,6 +4,8 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to maven-demo project,ok!!!");
+		System.out.prin("Welcome to maven-demo project,ok!!!");
 	}
+	
+	var a
 }
