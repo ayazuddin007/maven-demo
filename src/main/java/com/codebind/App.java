595753a9fe778ecdbddad77f43920352871fdbe6
Ayazuddin <46484289+ayazuddin007@users.noreply.github.com>
2019-10-16 14:48:16 +0530

    eeee

--- src/main/java/com/codebind/App.java
+++ src/main/java/com/codebind/App.java
@@ -2,9 +2,10 @@ package com.codebind;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void String[] args) {                   //main(
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to maven-demo project,ok!!!"
+		System.out.println("Welcome to maven-demo project,ok!!!");
+		//System.out.println("Welcome to maven-demo project,ok!!!");
 	}
 
 }
