e6ee79ecd91396bb2315c72758c03feb076c7c66
Ayazuddin <46484289+ayazuddin007@users.noreply.github.com>
2019-10-20 19:52:41 +0530

    error

--- src/main/java/com/codebind/App.java
+++ src/main/java/com/codebind/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {                   //main(
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to maven-demo project,ok!!!");
+		System.out.println("Welcome to maven-demo project,ok!!!"
 		//System.out.println("Welcome to maven-demo project,ok!!!");
 	}
 
